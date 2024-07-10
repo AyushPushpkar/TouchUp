b91ede9971f5c389b0fb8a5418ab1f98b9940008
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-07-10 18:59:56 +0530

    commit 2

--- app/src/main/java/com/example/touchup/RetrofitActivity.kt
+++ app/src/main/java/com/example/touchup/RetrofitActivity.kt
@@ -2,34 +2,55 @@ package com.example.touchup
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlin.coroutines.cancellation.CancellationException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 
 class RetrofitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_retrofit)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val mainView = findViewById<View>(R.id.main)
+        if (mainView != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(mainView) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        } else {
+            Log.e("Error", "Main view is null")
         }
 
-        val QuoteApi = RetrofitBuilder.getInstance().create(myInterface::class.java)
+        val quoteApi = RetrofitBuilder.getInstance().create(myInterface::class.java)
 
-        GlobalScope.launch {
-            val result = QuoteApi.getQuotes()
-            if (result != null){
-                Log.d("Result", result.body().toString())
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                val response: Response<QuoteList> = quoteApi.getQuotes()
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            Log.d("Result", it.toString())
+                        } ?: run {
+                            Log.e("Error", "Response body is null")
+                        }
+                    } else {
+                        Log.e("Error", "Error: ${response.errorBody()}")
+                    }
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Log.e("Exception", "Network request failed: ${e.message}", e)
+                }
             }
         }
-
     }
 }
